09dc2b84bf4e2a7d5a36430df4de471049cd5aa7
Normalizex <lanixoff@gmail.com>
2022-05-01 15:20:44 +0300

    Init Files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 0x-api-swap
-A Node.js library targeting v4 of the 0x DeFi aggregator API.
+A Node.js library targeting of the 0x DeFi aggregator API.
---
+++ src/types/IV1.ts
@@ -0,0 +1,17 @@
+// Interface for v1 api
+
+export interface QuoteParams {
+
+}
+
+export interface QuoteResponse {
+
+}
+
+export interface PriceParams {
+
+}
+
+export interface PriceResponse {
+	
+}
